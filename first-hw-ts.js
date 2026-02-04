fd9a792573274002364053d1caf310a21c43a41a
MychailoM <142823203+MychailoM@users.noreply.github.com>
2026-02-04 12:54:47 +0200

    04.02.2026

--- first-hw-ts.js
+++ first-hw-ts.js
@@ -1,11 +1,28 @@
-var inp = document.querySelector('input');
-var btn = document.querySelector('button');
-var text = document.querySelector('p');
-var arr = [];
-if (inp && btn && text) {
-    btn.addEventListener('click', function () {
-        text.textContent = inp.value;
-        arr.push(inp.value);
-        alert(arr);
-    });
+function converting(a) {
+    return a.toString();
 }
+console.log(converting(12));
+var shopping;
+shopping = ['laptop', 25000];
+shopping = ['phone', 15000];
+shopping = ['tablet', 10000];
+var tasks = [
+    { id: 1, title: "Зробити домашнє", status: "todo" },
+    { id: 2, title: "Прочитати книгу", status: "done" },
+    { id: 3, title: "Прибрати кімнату", status: "todo" }
+];
+var numbers = [1, 2, 3];
+function sumNumbers(numbers) {
+    return numbers.reduce(function (acc, num) { return acc + num; }, 0);
+}
+var result = sumNumbers(numbers);
+console.log(result);
+var items = [
+    { id: 1, name: "Apple" },
+    { id: 2, name: "Banana" },
+    { id: 3, name: "Orange" },
+];
+function findById(items, id) {
+    return items.find(function (item) { return item.id === id; });
+}
+console.log(findById(items, 2));
--- first-hw-ts.ts
+++ first-hw-ts.ts
@@ -1,12 +1,44 @@
-const inp = document.querySelector('input');
-const btn = document.querySelector('button');
-const text = document.querySelector('p');
-const arr: string[] = [];
-
-if (inp && btn && text) {
-  btn.addEventListener('click', () => {
-    text.textContent = inp.value;
-    arr.push(inp.value);
-    alert(arr)
-  });
+function converting(a: number): string {
+  return a.toString();
 }
+console.log(converting(12));
+
+let shopping: [string, number];
+shopping = ['laptop', 25000];
+shopping = ['phone', 15000];
+shopping = ['tablet', 10000];
+
+type Status = "todo" | "done";
+type Task = {
+  id: number;
+  title: string;
+  status: Status;
+};
+const tasks: Task[] = [
+  { id: 1, title: "Зробити домашнє", status: "todo" },
+  { id: 2, title: "Прочитати книгу", status: "done" },
+  { id: 3, title: "Прибрати кімнату", status: "todo" }
+];
+
+const numbers: number[] = [1, 2, 3];
+function sumNumbers(numbers: number[]): number {
+  return numbers.reduce((acc, num) => acc + num, 0);
+}
+const result = sumNumbers(numbers);
+console.log(result);
+
+
+type Item = {
+  id: number;
+  name: string;
+};
+const items: Item[] = [
+  { id: 1, name: "Apple" },
+  { id: 2, name: "Banana" },
+  { id: 3, name: "Orange" },
+];
+function findById(items: Item[], id: number): Item | undefined{
+  return items.find(item => item.id === id);
+}
+
+console.log(findById(items, 2))
