b8cb230c795f0ace1def5d5778b1c594ed4a8087
MychailoM <142823203+MychailoM@users.noreply.github.com>
2026-02-19 21:23:22 +0200

    /

--- first-hw-ts.js
+++ first-hw-ts.js
@@ -1,28 +1,155 @@
-function converting(a) {
-    return a.toString();
-}
-console.log(converting(12));
-var shopping;
-shopping = ['laptop', 25000];
-shopping = ['phone', 15000];
-shopping = ['tablet', 10000];
-var tasks = [
-    { id: 1, title: "Зробити домашнє", status: "todo" },
-    { id: 2, title: "Прочитати книгу", status: "done" },
-    { id: 3, title: "Прибрати кімнату", status: "todo" }
-];
-var numbers = [1, 2, 3];
-function sumNumbers(numbers) {
-    return numbers.reduce(function (acc, num) { return acc + num; }, 0);
-}
-var result = sumNumbers(numbers);
-console.log(result);
-var items = [
-    { id: 1, name: "Apple" },
-    { id: 2, name: "Banana" },
-    { id: 3, name: "Orange" },
-];
-function findById(items, id) {
-    return items.find(function (item) { return item.id === id; });
-}
-console.log(findById(items, 2));
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+//============== №1 ===========
+console.log('-=-=+=-=- №1 -=-=+=-=-');
+var Car = /** @class */ (function () {
+    function Car(car) {
+        this.brand = car;
+    }
+    Car.prototype.showBrand = function () {
+        console.log("Brand: ".concat(this.brand));
+    };
+    return Car;
+}());
+var car = new Car("Audi");
+car.showBrand();
+//============== №2 ===========
+console.log('-=-=+=-=- №2 -=-=+=-=-');
+var User = /** @class */ (function () {
+    function User(name, pass) {
+        this.name = name;
+        this.password = pass;
+    }
+    User.prototype.checkPassword = function (pass) {
+        return this.password === pass;
+    };
+    return User;
+}());
+var user = new User("Ivan", "12345");
+console.log(user.checkPassword("12345"));
+console.log(user.checkPassword("54321"));
+//============== №3 ===========
+console.log('-=-=+=-=- №3 -=-=+=-=-');
+var Animal = /** @class */ (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    Animal.prototype.makeSound = function () {
+        console.log("Meow");
+    };
+    return Animal;
+}());
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Dog.prototype.makeSound = function () {
+        _super.prototype.makeSound.call(this); //Функція Super потрібна для того щоб викликати методи класу Animal
+        console.log("Woof");
+    };
+    return Dog;
+}(Animal));
+var dog = new Dog("Berta");
+dog.makeSound();
+//============== №4 ===========
+console.log('-=-=+=-=- №4 -=-=+=-=-');
+var Person = /** @class */ (function () {
+    function Person(age) {
+        this._age = 0;
+        this.age = age;
+    }
+    Object.defineProperty(Person.prototype, "age", {
+        get: function () {
+            return this._age;
+        },
+        set: function (value) {
+            if (value < 0) {
+                console.log('Error');
+                return;
+            }
+            this._age = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Person;
+}());
+var person = new Person(25);
+console.log(person.age);
+person.age = -5;
+console.log(person.age);
+person.age = 30;
+console.log(person.age);
+//============== №5 ===========
+console.log('-=-=+=-=- №5 -=-=+=-=-');
+var Student = /** @class */ (function () {
+    function Student(name) {
+        this.name = name;
+        Student.count++;
+    }
+    Student.showCount = function () {
+        console.log("\u043A\u0456\u043B\u044C\u043A\u0456\u0441\u0442\u044C \u0441\u0442\u0443\u0434\u0435\u043D\u0442\u0456\u0432: ".concat(Student.count));
+    };
+    Student.count = 0;
+    return Student;
+}());
+var s1 = new Student("Ivan");
+var s2 = new Student("Olena");
+var s3 = new Student("Dmytro");
+Student.showCount();
+console.log(Student.count);
+//============== №6 ===========
+console.log('-=-=+=-=- №6 -=-=+=-=-');
+var Shape = /** @class */ (function () {
+    function Shape() {
+    }
+    Shape.prototype.describe = function () {
+        console.log('Геометрична фігура');
+    };
+    return Shape;
+}());
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    function Circle(radius) {
+        var _this = _super.call(this) || this;
+        _this.radius = radius;
+        return _this;
+    }
+    Circle.prototype.getArea = function () {
+        return parseFloat((Math.PI * Math.pow(this.radius, 2)).toFixed(1));
+    };
+    return Circle;
+}(Shape));
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle(width, height) {
+        var _this = _super.call(this) || this;
+        _this.width = width;
+        _this.height = height;
+        return _this;
+    }
+    Rectangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}(Shape));
+var circle = new Circle(4);
+circle.describe();
+console.log("Площа кола:", circle.getArea());
+var rectangle = new Rectangle(4, 6);
+rectangle.describe();
+console.log("Площа прямокутника:", rectangle.getArea());
--- first-hw-ts.ts
+++ first-hw-ts.ts
@@ -1,44 +1,153 @@
-function converting(a: number): string {
-  return a.toString();
-}
-console.log(converting(12));
+//============== №1 ===========
+console.log('-=-=+=-=- №1 -=-=+=-=-')
+class Car{
+  brand: string;
 
-let shopping: [string, number];
-shopping = ['laptop', 25000];
-shopping = ['phone', 15000];
-shopping = ['tablet', 10000];
-
-type Status = "todo" | "done";
-type Task = {
-  id: number;
-  title: string;
-  status: Status;
-};
-const tasks: Task[] = [
-  { id: 1, title: "Зробити домашнє", status: "todo" },
-  { id: 2, title: "Прочитати книгу", status: "done" },
-  { id: 3, title: "Прибрати кімнату", status: "todo" }
-];
-
-const numbers: number[] = [1, 2, 3];
-function sumNumbers(numbers: number[]): number {
-  return numbers.reduce((acc, num) => acc + num, 0);
-}
-const result = sumNumbers(numbers);
-console.log(result);
-
-
-type Item = {
-  id: number;
-  name: string;
-};
-const items: Item[] = [
-  { id: 1, name: "Apple" },
-  { id: 2, name: "Banana" },
-  { id: 3, name: "Orange" },
-];
-function findById(items: Item[], id: number): Item | undefined{
-  return items.find(item => item.id === id);
+  constructor(car: string){
+    this.brand = car
+  }
+  showBrand(){
+    console.log(`Brand: ${this.brand}`)
+  }
 }
 
-console.log(findById(items, 2))
+const car = new Car("Audi")
+car.showBrand();
+
+
+//============== №2 ===========
+console.log('-=-=+=-=- №2 -=-=+=-=-')
+class User{
+  public name: string;
+  private password: string;
+
+  constructor(name: string, pass: string){
+    this.name = name;
+    this.password = pass;
+  }
+
+  checkPassword(pass: string): boolean{
+    return this.password === pass
+  }
+}
+
+const user = new User("Ivan", "12345");
+console.log(user.checkPassword("12345"));
+console.log(user.checkPassword("54321"));
+
+
+//============== №3 ===========
+console.log('-=-=+=-=- №3 -=-=+=-=-')
+class Animal{
+  protected name: string;
+
+  constructor(name: string){
+    this.name = name
+  }
+
+    makeSound(){
+    console.log("Meow")
+  }
+}
+
+class Dog extends Animal{
+  makeSound(){
+    super.makeSound();//Функція Super потрібна для того щоб викликати методи класу Animal
+    console.log("Woof");
+  }
+}
+
+const dog = new Dog("Berta");
+dog.makeSound();
+
+
+//============== №4 ===========
+console.log('-=-=+=-=- №4 -=-=+=-=-')
+class Person{
+  private _age: number = 0;
+
+  constructor(age: number){
+    this.age = age
+  }
+
+  set age(value: number){
+    if(value < 0){
+      console.log('Error');
+      return
+    }
+    this._age = value;
+  }
+
+  get age(): number{
+    return this._age;
+  }
+}
+
+const person = new Person(25);
+console.log(person.age); 
+person.age = -5;
+console.log(person.age); 
+person.age = 30;
+console.log(person.age); 
+
+//============== №5 ===========
+console.log('-=-=+=-=- №5 -=-=+=-=-')
+class Student{
+  static count: number = 0;
+
+  constructor(public name: string){
+    Student.count++
+  }
+
+  static showCount(){
+    console.log(`кількість студентів: ${Student.count}`)
+  }
+}
+
+const s1 = new Student("Ivan");
+const s2 = new Student("Olena");
+const s3 = new Student("Dmytro");
+
+Student.showCount();
+
+console.log(Student.count);
+
+//============== №6 ===========
+console.log('-=-=+=-=- №6 -=-=+=-=-')
+abstract class Shape {
+  abstract getArea(): number;
+  describe(){
+    console.log('Геометрична фігура')
+  }
+  constructor() {
+    
+  }
+}
+
+class Circle extends Shape{
+  constructor(public radius: number){
+    super()
+  }
+
+  getArea(): number{
+    return parseFloat((Math.PI * this.radius ** 2).toFixed(1));
+  }
+}
+
+class Rectangle extends Shape{
+  constructor(public width: number, public height: number){
+    super()
+  }
+
+  getArea(): number{
+    return this.width * this.height;
+  }
+}
+
+const circle = new Circle(4);
+circle.describe();
+console.log("Площа кола:", circle.getArea());
+
+const rectangle = new Rectangle(4, 6);
+rectangle.describe();
+console.log("Площа прямокутника:", rectangle.getArea());
